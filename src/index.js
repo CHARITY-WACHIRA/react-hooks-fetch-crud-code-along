5ef77fb15e389713830346f65c18c7d971f43d21
CHARITY-WACHIRA <kouwywachira@gmail.com>
2023-07-25 13:58:37 +0300

    finished code-along

--- src/index.js
+++ src/index.js
@@ -2,5 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+function Item({ item }) {
+    // Add function to handle button click
+    function handleAddToCartClick() {
+      console.log("clicked item:", item);
+    }
+  
+    return (
+      <li className={item.isInCart ? "in-cart" : ""}>
+        <span>{item.name}</span>
+        <span className="category">{item.category}</span>
+        {/* add the onClick listener */}
+        <button
+          className={item.isInCart ? "remove" : "add"}
+          onClick={handleAddToCartClick}
+        >
+          {item.isInCart ? "Remove From" : "Add to"} Cart
+        </button>
+        <button className="remove">Delete</button>
+      </li>
+    );
+  }
 
 ReactDOM.render(<App />, document.getElementById("root"));
